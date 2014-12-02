a5944538e89a176b2f1ecdef6e13179f9c242779
Daniel Horlitz <dhorlitz@gmail.com>
2014-12-02 16:17:27 -0500

    Chapter 7 - authentication against static array with bcrypt

--- server-auth.js
+++ server-auth.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var jwt = require('jwt-simple')
 var _ = require('lodash')
+var bcrypt = require('bcrypt')
 var app = express()
 app.use(require('body-parser').json())
 
 var users = [
-  {username: 'dickeyxxx', password: 'pass'}
+  {username: 'dickeyxxx', password: '$2a$10$Hryq0/xSP6m2rcDWgmGOIe/xHR3ZWu.wqWPD17Df5h8QCvcgiqWO2'}
 ]
 
 var secretKey = 'supersecretkey'
@@ -14,19 +15,20 @@ function findUserByUsername(username) {
   return _.find(users, {username: username})
 }
 
-function validateUser(user, password) {
-  return user.password === password
+function validateUser(user, password, cb) {
+  bcrypt.compare(password, user.password, cb)
 }
 
 app.post('/session', function (req, res) {
   var user = findUserByUsername(req.body.username)
 
-  if (!validateUser(user, req.body.password)) {
-    return res.send(401) // Unauthorized
-  }
-
-  var token = jwt.encode({username: user.username}, secretKey)
-  res.json(token)
+  validateUser(user, req.body.password, function (err, valid) {
+    if (err || !valid) {
+      return res.send(401)
+    }
+    var token = jwt.encode({username: user.username}, secretKey)
+    res.json(token)
+  })
 })
 
 app.get('/user', function (req, res) {
